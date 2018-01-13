1f1b513defc7d3ec17ecf8fd4f4554b535756158
asakhnenko <asakhnenko@uos.de>
2018-01-13 13:31:09 +0100

    bash script 1 version

---
+++ blatt3/load_lkm.sh
@@ -0,0 +1,12 @@
+# !/bin/bash
+echo "Preparing to load LKM"
+echo "Compiling LKM"
+make
+echo "Removing old version"
+sudo rmmod reverse.ko
+echo "Inserting new version"
+sudo insmod reverse.ko
+sudo chmod 666 /dev/reverse
+echo "Compiling test file"
+gcc -o test test.c
+echo "Done"

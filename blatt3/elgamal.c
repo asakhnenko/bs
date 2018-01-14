b2d42ecf742d66e05b98d57c1340f7f44b83251b
Joshua Wiebe <joshua@debian.joshuawiebe>
2018-01-14 09:10:43 -0600

    elgamal update

--- blatt3/elgamal.c
+++ blatt3/elgamal.c
@@ -44,20 +44,17 @@ int main(int argc, char **argv)
 	}
 
 	// Iterate..
-	for (nrcnt = 10; nrcnt < 59; ++nrcnt) {
+	for (nrcnt = 1; nrcnt < 59; ++nrcnt) {
 		printf("enter loop\n");
 
 		// encrypting the original number
 		encrypted = encrypt(nrcnt);
-		printf("CP1\n");
 		// writing the encrypted number to device file
 		write(fd, &encrypted, sizeof(encrypted));
-		printf("CP2 %d\n", encrypted);
 		// reading the decrypted number from the device file
 		read(fd, &decrypted, sizeof(decrypted));
-		printf("CP3\n");
 
-		printf("%d\n", decrypted);
+		printf("decrypted: %d\n", decrypted);
 		
 		sleep(1);
 	}
@@ -81,63 +78,3 @@ int main(int argc, char **argv)
 
 	return 0;
 }
-
-/**
- * @brief Prints usage information
- * @details Prints information to run the load script and some 
- * additional message that can be passed as an argument.
- * 
- * @param message 	The message that precedes the load script information. If NULL 
- * 					or an empty string is passed, it will be skipped
- */
-
- /**
-void print_module_usage(char *message)
-{
-	char *usage = "In order to test the module, please run the shell script 'load_lkm.sh' before executing this file.\n";
-
-	if (message && strlen(message) > 0) {
-		printf("%s\n%s", message, usage);
-	} else {
-		printf(usage);
-	}
-}
-*/
-/**
- * @brief Generates a public key
- * @details Computes a public key from the private key and the cyclic group (p, g) using the Elgamal Method
- * 
- * @param generator 	The generator (g) as part of the cyclic group
- * @param secret 		The private key of this instance
- * @param order 		The order (p) as part of the cyclic group
- * 
- * @return A public key that can be shared with the communication partners
- */
- /**
-param_t generate_openkey(param_t generator, param_t secret, param_t order)
-{
-	return (param_t) mod_exp(generator, secret, order);
-}
-*/
-/**
- * @brief Encrypts a number
- * @details Encrypts a positive number using the private key, the public key 
- * of the receiver and the order of the cyclic group. This encryption uses 
- * the Elgamal Encryption.
- * 
- * @param openkey_receiver 	The public key of the communication partner
- * @param secret 			The private key of this instance
- * @param nrcnt 			The message (number) that needs to be encrypted
- * @param order 			The order (part of the cyclic group)
- * 
- * @return The encrypted message (number)
- */
- /**
-number_t encrypt(param_t openkey_receiver, param_t secret, number_t nrcnt, param_t order)
-{
-	number_t encrypted = mod_exp(openkey_receiver, secret, order);
-	encrypted *= nrcnt % order;
-
-	return encrypted;
-}
-*/
